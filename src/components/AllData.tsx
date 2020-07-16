983d0480b7aa5732aa123299a02649b1257c5c64
Stephen Koch <stephen.koch@postlight.com>
2020-07-16 10:21:33 -0400

    feat: major upgrade

--- src/components/AllData.tsx
+++ src/components/AllData.tsx
@@ -1,70 +1,45 @@
 import axios from 'axios';
 import moment from 'moment';
-import { find } from 'lodash-es';
-import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
 import React, { memo, useState, useEffect } from 'react';
-import { VictoryAxis, VictoryLine, VictoryChart } from 'victory';
 
-import formatNumber, { statesData, dataPoints, DataPoint } from '../utils/misc';
-import { Header, Header2, PageLabel, StyledLink } from '../styles/Styles';
+import formatNumber, { dataPoints, DataPoint, statesData } from '../utils/misc';
+// import USChart from './USChart';
+import { useParams } from 'react-router-dom';
+import { find } from 'lodash-es';
+import { Typography } from '@material-ui/core';
+import MyChart from './MyChart';
 
-const Root = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Charts = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-
-  margin-top: 4rem;
-`;
-
-const ChartWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  margin: 1rem;
-  padding-top: 2rem;
-  /* border: 1px solid #ff4f00; */
-  background-color: #fff;
-  box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
-    0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06),
-    0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086),
-    0 100px 80px rgba(0, 0, 0, 0.12);
-`;
-
-// const getStyles = () => ({
-//   parent: {
-//     background: '#ccdee8',
-//     boxSizing: 'border-box',
-//     display: 'inline',
-//     padding: 0,
-//     fontFamily: "'Fira Sans', sans-serif",
-//   },
-// });
+// interface Props {
+//   state: string;
+// }
 
 const AllData = () => {
   const { slug } = useParams();
+  // console.log('slug', slug);
   const [loading, setLoading] = useState(true);
+  const [currentSlug, setCurrentSlug] = useState(slug);
+  // TODO: fix this any situ
   const [data, setData] = useState<any[] | undefined>(undefined);
-
-  const state: string | undefined = find(statesData, [
+  const stateCode: string | undefined = find(statesData, ['slug', currentSlug])
+    ?.code;
+  const stateFullName: string | undefined = find(statesData, [
     'slug',
-    slug,
-  ])?.code.toLowerCase();
+    currentSlug,
+  ])?.state;
 
-  const stateFullName: string | undefined = find(statesData, ['slug', slug])
-    ?.state;
+  useEffect(() => {
+    if (slug !== setCurrentSlug) {
+      setCurrentSlug(slug);
+      setLoading(true);
+      setData(undefined);
+    }
+  }, [slug]);
 
   useEffect(() => {
     const getData = async () => {
       try {
         const response = await axios.get(
-          `https://covidtracking.com/api/v1/states/${state}/daily.json`,
+          `https://covidtracking.com/api/v1/states/${stateCode?.toLowerCase()}/daily.json`,
         );
         setData(response.data);
         setLoading(false);
@@ -78,46 +53,45 @@ const AllData = () => {
     }
   });
 
-  if (loading) return null;
+  if (loading || !data)
+    return (
+      <>
+        <Typography variant="h3">{stateFullName}</Typography>
+        <Typography variant="h4">Loading...</Typography>
+      </>
+    );
   // console.log('data', data);
 
-  const datedData = data?.map(d => {
-    const foo = moment(String(d.date));
+  const datedData = data.map(d => {
+    const date = moment(String(d.date));
     return {
       ...d,
-      date: new Date(foo.year(), foo.month(), foo.date()),
+      date: new Date(date.year(), date.month(), date.date()),
     };
   });
 
   return (
-    <Root>
-      <StyledLink to={`/current/${slug}`}>View Current Data</StyledLink>
-      <PageLabel>All Data</PageLabel>
-      <Header>{stateFullName}</Header>
-      <Charts>
-        {dataPoints.map(({ point, label }: DataPoint) => (
-          <ChartWrapper key={label}>
-            <Header2>{label}</Header2>
-            <VictoryChart
-              padding={{ top: 50, left: 75, right: 50, bottom: 50 }}
-              // theme={VictoryTheme.material}
-              width={500}
-              height={300}
-            >
-              <VictoryLine
-                style={{ data: { stroke: '#FF4F00' } }}
-                data={datedData}
-                x="date"
-                y={point}
-                // interpolation="natural"
-              />
-              <VictoryAxis dependentAxis tickFormat={t => formatNumber(t)} />
-              <VictoryAxis tickFormat={t => moment(t).format('M-DD')} />
-            </VictoryChart>
-          </ChartWrapper>
-        ))}
-      </Charts>
-    </Root>
+    <>
+      <Typography variant="h3" gutterBottom>
+        {stateFullName}
+      </Typography>
+      {dataPoints.map(({ point, label }: DataPoint) => (
+        <MyChart
+          key={label}
+          title={`${label} - ${formatNumber(datedData[0][point])}`}
+          label={label}
+          chartData={datedData.map(d => [new Date(d.date), d[point]])}
+        />
+        // <USChart
+        //   key={label}
+        //   label={`${label} - ${formatNumber(datedData[0][point])}`}
+        //   data={datedData.map(d => ({
+        //     x: d.date,
+        //     y: d[point],
+        //   }))}
+        // />
+      ))}
+    </>
   );
 };
 
--- src/components/CurrentData.tsx
+++ src/components/CurrentData.tsx
@@ -1,104 +1,53 @@
-import axios from 'axios';
 import moment from 'moment';
-import { find } from 'lodash-es';
-import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
-import React, { memo, useState, useEffect } from 'react';
-
-import {
-  Header,
-  Header2,
-  Card,
-  PageLabel,
-  fadeIn,
-  StyledLink,
-} from '../styles/Styles';
+import React, { memo } from 'react';
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import CardContent from '@material-ui/core/CardContent';
 
 import { IStateData } from '../types';
 
-import formatNumber, { statesData, DataPoint, dataPoints } from '../utils/misc';
+import formatNumber, { dataPoints, DataPoint } from '../utils/misc';
 
-const Root = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    margin: '2rem 0',
+  },
+  uppercase: {
+    textTransform: 'uppercase',
+  },
+});
 
-  ${fadeIn};
-`;
-
-const Cards = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-
-  margin-top: 2rem;
-`;
-
-const Label = styled.h3`
-  margin: 0;
-  font-size: 1.8rem;
-`;
-
-const Value = styled.h3`
-  margin: 0;
-  font-family: 'basier';
-  font-size: 1.6rem;
-`;
-
-const CurrentData = () => {
-  const { slug } = useParams();
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<IStateData | undefined>(undefined);
-
-  const state: string | undefined = find(statesData, [
-    'slug',
-    slug,
-  ])?.code.toLowerCase();
-
-  const stateFullName: string | undefined = find(statesData, ['slug', slug])
-    ?.state;
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get(
-          `https://covidtracking.com/api/v1/states/${state}/current.json`,
-        );
-        setData(response.data);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    if (loading && !data) {
-      getData();
-    }
-  });
-
-  if (loading || !data) return null;
-
-  const day = moment(data.dateChecked);
+interface Props {
+  name: string;
+  state: IStateData;
+}
+const CurrentData = ({ name, state }: Props) => {
+  const classes = useStyles();
+  const day = moment(state.dateChecked);
 
   return (
-    <Root>
-      <StyledLink to={`/all/${slug}`}>View Charts</StyledLink>
-      <PageLabel>Current Data</PageLabel>
-      <Header>{stateFullName}</Header>
-      <Header2>{day.format('MMM Do, YYYY')}</Header2>
-      <Cards>
-        {dataPoints.map(({ point, label }: DataPoint) => {
-          return (
-            <Card key={label}>
-              <Label>{label}</Label>
-              <Value>
-                {formatNumber(Number(data[point as keyof IStateData]))}
-              </Value>
-            </Card>
-          );
-        })}
-      </Cards>
-    </Root>
+    <>
+      <Typography variant="h2" gutterBottom>
+        {name}
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        {`Last checked: ${day.format('MMM Do, YYYY')}`}
+      </Typography>
+      {dataPoints.map(({ point, label }: DataPoint) => {
+        return (
+          <Card key={label} className={classes.card}>
+            <CardContent>
+              <Typography variant="h6">{label}</Typography>
+              <Typography variant="button" display="block">
+                {formatNumber(Number(state[point as keyof IStateData]))}
+              </Typography>
+            </CardContent>
+          </Card>
+        );
+      })}
+    </>
   );
 };
 
